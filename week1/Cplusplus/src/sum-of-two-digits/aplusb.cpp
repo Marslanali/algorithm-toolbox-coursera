b1d5b95ac1b37f50697936b2ca7b454820b26564
marslanali <marslanali800@gmail.com>
2020-12-09 14:02:17 +0500

    updating changes

--- week1/Cplusplus/src/sum-of-two-digits/aplusb.cpp
+++ week1/Cplusplus/src/sum-of-two-digits/aplusb.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
-int32_t sum_of_two_digits(int32_t first_digit, int32_t second_digit) {
+int32_t sum_of_two_digits(int32_t first_digit, int32_t second_digit)
+{
   return first_digit + second_digit;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   int32_t a = 0;
   int32_t b = 0;
   std::cin >> a;
---
+++ week1/Python/sum-of-two-digits/aplusb.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def sum_of_two_digits(_a, _b):
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    return _a + _b
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('sum_of_two_digits')
+    a = float(input())
+    b = float(input())
+    print(sum_of_two_digits(a, b))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
